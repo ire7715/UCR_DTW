c1223926d67ae81f875cc2a07618dbc5a214b392
Ire Sun <ire7715@gmail.com>
2014-12-14 12:15:28 +0800

    first, UCR in Ruby style

---
+++ UCR_DTW.rb
@@ -0,0 +1,475 @@
+class Trillion
+
+	def self.dist (x, y)
+		return (x-y)**2
+	end
+
+	def self.upper_lower_lemire (t, len, r)
+		du = Array.new()
+		dl = Array.new()
+		u = Array.new(len)
+		l = Array.new(len)
+
+		du.push(0)
+		dl.push(0)
+
+		(1 .. len-1).each{ |i|
+
+			if (i > r)
+				u[i-r-1] = t[du.first.to_i]
+				l[i-r-1] = t[dl.first.to_i]
+			end
+
+			if (t[i] > t[i-1])
+				du.pop
+				while (!(du.empty?) && t[i] > t[du.last.to_i])
+					du.pop
+				end
+			else
+				dl.pop
+				while (!(dl.empty?) && t[i] < t[dl.last.to_i])
+					dl.pop
+				end
+			end
+			du.push(i)
+			dl.push(i)
+
+			# i - r - 1 == r + du.first
+			if ( i == 2 * r + 1 + du.first)
+				du.shift
+			elsif ( i == 2 * r + 1 + dl.first)
+				dl.shift
+			end
+		}
+
+		(len .. len+r).each{ |i|
+			u[i-r-1] = t[du.first.to_i]
+			l[i-r-1] = t[dl.first.to_i]
+			if (i - du.first.to_i >= 2 * r + 1)
+				du.shift
+			end
+			if (i - dl.first.to_i >= 2 * r + 1)
+				dl.shift
+			end
+		}
+
+		return l, u
+	end
+
+	def self.lb_kim_hierarchy(t, q, j, mean, std, bsf = Float::INFINITY)
+		lb = 0
+
+		# 1st points at front and back
+		x0 = (t[j] - mean) / std
+		y0 = (t[(j + q.length - 1)] - mean) / std
+		lb = dist(x0, q.first) + dist(y0, q.last)
+		if( lb >= bsf )	
+			return lb
+		end
+
+		# 2 points at front
+		x1 = (t[(j + 1)] - mean) / std
+		d = [dist(x1, q.first), dist(x0, q[1]), dist(x1, q[1])].min
+		lb += d
+		if (lb >= bsf)
+			return lb
+		end
+
+    	# 2 points at back
+    	y1 = (t[(j + q.length - 2)] - mean) / std
+    	d = [dist(y1, q.last), dist(y0, q[-2]), dist(y1, q[-2])].min
+    	lb += d
+    	if (lb >= bsf)
+    		return lb
+    	end
+
+    	# 3 points at front
+    	x2 = (t[(j + 2)] - mean) / std
+    	d = [dist(x0,q[2]), dist(x1, q[2]), dist(x2,q[2]), dist(x2,q[1]), dist(x2,q.first)].min
+    	lb += d
+    	if (lb >= bsf)
+    		return lb
+    	end
+
+    	# 3 points at back
+    	y2 = (t[(q.length - 3 + j)] - mean) / std
+    	d = [dist(y0, q[-3]), dist(y1, q[-3]), dist(y2, q[-3]), dist(y2, q[-2]), dist(y2, q[-1])].min
+    	lb += d
+
+    	return lb
+	end
+
+	def self.lb_keogh_cumulative(order, t, uo, lo, cb, j, m, mean, std, bsf = Float::INFINITY)
+		lb = 0
+
+		m.times{ |i|
+
+			x = (t[(j + order[i]).to_i] - mean) / std
+			d = 0
+			if (x > uo[i])
+				d = dist(x, uo[i])
+			elsif (x < lo[i])
+				d = dist(x, lo[i])
+			end
+			lb += d
+			cb[order[i]] = 	d
+
+			if(lb >= bsf)
+				break
+			end
+		}
+
+		return lb
+	end
+
+	def self.lb_keogh_data_cumulative (order, qo, cb, l, u, j, mean, std, bsf = Float::INFINITY)
+		lb = 0
+		order.length.times{ |i|
+			uu = (u[j + order[i]] - mean) / std
+			ll = (l[j + order[i]] - mean) / std
+			d = 0
+
+			if (qo[i] > uu)
+				d = dist(qo[i], uu)
+			elsif (qo[i] < ll)
+				d = dist(qo[i], ll)
+			end
+			lb += d
+			cb[order[i]] = d
+
+			if(lb >= bsf)
+				break
+			end
+		}
+		return lb
+	end
+
+	def self.dtw(a_seq, b_seq, cb, m, r, bsf = Float::INFINITY)
+		cost = Array.new(2 * r + 1, Float::INFINITY)
+		cost_prev = Array.new(2 * r + 1, Float::INFINITY)
+		k = 0
+
+		m.times{ |i|
+			k = [0, r - i].max
+			min_cost = Float::INFINITY
+
+			j = [0,i-r].max
+			while(j <= [m-1,i+r].min)
+				if( i == 0 && j == 0)
+					min_cost = cost[k] = dist(a_seq[0], b_seq[0])
+				else
+					if ((j-1<0)||(k-1<0))
+						y = Float::INFINITY
+            		else
+            			y = cost[k-1]
+            		end
+            		if ((i-1<0)||(k+1>2*r))
+            			x = Float::INFINITY
+            		else
+						x = cost_prev[k+1]
+            		end
+            		if ((i-1<0)||(j-1<0))
+            			z = Float::INFINITY
+            		else
+            			z = cost_prev[k]
+            		end
+
+            		cost[k] = [x, y, z].min + dist(a_seq[i], b_seq[j])
+
+            		if(cost[k] < min_cost)
+            			min_cost = cost[k]
+            		end
+				end
+				k += 1
+				j += 1
+			end
+
+			if(i+r < m-1 && min_cost + cb[i+r+1] >= bsf)
+				return min_cost + cb[i+r+1]
+			end
+
+			cost_tmp = cost
+        	cost = cost_prev
+        	cost_prev = cost_tmp
+		}
+
+		k -= 1
+
+		return cost_prev[k]
+	end
+
+	EPOCH = 100000
+
+	def initialize(dat, query, window_rate, dontSort)
+
+		loc = 0
+		kim = keogh = keogh2 = 0
+		dist = lb_kim = lb_k = lb_k2 = 0.0
+
+		data_name = dat
+		query_name = query
+
+		ex = ex2 = 0.0
+		bsf = Float::INFINITY
+		i = j = 0
+
+		start = Time.now
+
+		# read query
+		puts "Read query"
+		qp = File.open(query_name, "r")
+		q = Array.new()
+		qp.read.split(" ").each{ |x| 
+			q.push(x.to_f)
+			ex += q.last
+			ex2 += q.last**2
+		}
+		qp.close
+
+		mean = ex / q.length
+		std = (ex2 / q.length - mean ** 2) ** 0.5
+
+		# normalize
+		puts "Normalize query"
+		q.map!{ |x|
+			(x - mean) / std
+		}
+
+		if(window_rate <= 1)	# wraping window
+			r = (window_rate*q.length).floor
+		else
+			r = window_rate.floor
+		end
+
+		# calculating the envelop of q
+		puts "Calculating the envelop of q"
+		l, u = Trillion::upper_lower_lemire(q, q.length, r)
+
+		q_tmp = q.each_with_index.map{ |x, i|
+			IndexObj.new(i, x)
+		}
+
+		order = Array.new()
+		qo = Array.new()
+		lo = Array.new()
+		uo = Array.new()
+
+		if(!dontSort)
+			puts "Sorting"
+
+			q_tmp.sort!{ |a, b|
+				(b.value.abs) <=> (a.value.abs)
+			}
+
+			q_tmp.each{ |x|
+				order.push(x.index)
+				qo.push(q[x.index])
+				uo.push(u[x.index])
+				lo.push(l[x.index])
+			}
+		else
+			q.length.times{ |i|
+				order.push(i)
+				qo.push(q[i])
+				uo.push(u[i])
+				lo.push(l[i])
+			}
+		end
+
+		cb = Array.new(q.length, 0)
+		cb1 = Array.new(q.length, 0)
+		cb2 = Array.new(q.length, 0)
+
+		i = j =0
+		ex = ex2 = 0.0
+		done = false
+		it = ep = k = 0
+
+		tp = File::open(data_name, "r")
+		tdata = tp.read.split(" ").map{ |x|
+			x.to_f
+		}
+		tp.close
+
+		buffer = Array.new(EPOCH)
+		t = Array.new(q.length * 2)
+		tz = Array.new(q.length * 2)
+
+		puts "Searching"
+
+		while(!done)
+			ep = 0
+			if (it == 0)
+				(q.length-1).times{ |k|
+					if(tdata.length != 0)
+						buffer[k] = tdata.shift
+					end
+				}
+			else
+				(q.length-1).times{ |k|
+					buffer[k] = buffer[EPOCH - q.length + 1 + k]
+				}
+			end
+
+			ep = q.length - 1
+			while(ep < EPOCH && tdata.length != 0)
+				buffer[ep] = tdata.shift
+				ep += 1
+			end
+
+			if(ep <= q.length - 1)
+				done = true
+			else
+				l_buff, u_buff = Trillion::upper_lower_lemire(buffer, ep, r)
+
+				if (it % (1000000/(EPOCH - q.length + 1)) == 0 )
+					print "."
+				end
+
+				ex = ex2 = 0
+				ep.times{|i|
+					d = buffer[i]
+
+					ex += d
+					ex2 += d ** 2
+
+					t[i % q.length] = d
+					t[(i % q.length) + q.length] = d
+
+					if( i >= q.length - 1)
+						mean = ex / q.length
+						std = (ex2 / q.length - mean ** 2) ** 0.5
+
+						j = (i + 1) % q.length
+
+						iCap = i - (q.length - 1)
+
+						lb_kim = Trillion::lb_kim_hierarchy(t, q, j, mean, std, bsf)
+
+						if(lb_kim < bsf)
+							lb_k = Trillion::lb_keogh_cumulative(order, t, uo, lo, cb1, j, q.length, mean, std, bsf)
+
+							if(lb_k < bsf)
+
+								lb_k2 = Trillion::lb_keogh_data_cumulative(order, qo, cb2, l_buff, u_buff, iCap, mean, std, bsf)
+
+								if (lb_k2 < bsf)
+
+									if(lb_k > lb_k2)
+										cb[q.length - 1] = cb1[q.length - 1]
+										(q.length - 2).downto(0){|k|
+											cb[k] = cb[k + 1] + cb1[k]
+										}
+									else
+										cb[q.length - 1] = cb2[q.length - 1]
+										(q.length - 2).downto(0){|k|
+											cb[k] = cb[k + 1] + cb2[k]
+										}
+									end
+
+									tz = t[j .. j + q.length - 1].map{ |x|
+										(x - mean) / std
+									}
+
+									dist = Trillion::dtw(tz, q, cb, q.length, r, bsf)
+
+									if(dist < bsf)
+										bsf = dist
+										loc = it * (EPOCH - q.length + 1) + i - q.length + 1
+									end
+
+								else
+									keogh2 += 1
+								end
+							else
+								keogh += 1
+							end
+						else
+							kim += 1
+						end
+
+						ex -= t[j]
+						ex2 -= t[j] ** 2
+					end
+				}
+
+				if (ep < EPOCH)
+					done = true
+				else
+					it += 1
+				end
+			end
+
+		end
+
+		finish = Time.now
+
+		puts " "
+
+		@location = loc
+		puts "Location: #{loc}"
+		dist = bsf ** 0.5
+		puts "Distance: #{dist}"
+		i = it * (EPOCH - q.length + 1) + ep
+		puts "Data scanned: #{i}"
+		exeTime = finish - start
+		puts "Execution time: #{exeTime}"
+		puts " "
+
+		kim_perc = 100.0 * kim / i
+		puts "Pruned by LB_Kim: #{kim_perc}"
+		keogh_perc = 100.0 * keogh / i
+		puts "Pruned by LB_Keogh: #{keogh_perc}"
+		keogh2_perc = 100.0 *keogh2 / i
+		puts "Pruned by LB_Keogh2: #{keogh2_perc}"
+		dtw_prec = 100.0 - (kim_perc + keogh_perc + keogh2_perc)
+		puts "DTW Calcuation  : #{dtw_prec}"
+	end
+
+	def location
+		@location
+	end
+end
+
+class IndexObj
+	def initialize(i, val)
+		@index = i
+		@value = val
+	end
+
+	def index
+		@index
+	end
+
+	def value
+		@value
+	end
+end
+
+if __FILE__ == $PROGRAM_NAME
+	if (ARGV.length < 1 || ARGV[0] == nil)
+		puts "Need data file name as 1st parameter."
+		exit
+	end
+
+	if (ARGV.length < 2 || ARGV[1] == nil)
+		puts "Need query file name as 2nd parameter."
+		exit
+	end
+
+	if (ARGV.length < 3 || ARGV[2] == nil)
+		puts "Need window rate as 3rd parameter."
+		exit
+	end
+
+	dontSort = false
+
+	if (ARGV.length > 3)
+		ARGV[3 .. ARGV.length - 1].each{ |x|
+			dontSort = true if(x == "-ns")
+		}
+	end
+
+	Trillion.new(ARGV[0], ARGV[1], ARGV[2].to_f, dontSort)
+
+end
